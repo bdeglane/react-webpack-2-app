e56e0316967509d3e1a5aefc61dfb24d12cd9bce
Benoit Deglane <benoit.deglane@netapsys.fr>
2017-02-22 10:41:46 +0100

    clean webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,43 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = process.env.NODE_ENV === 'development';
+
 module.exports = {
-  entry: path.resolve(path.join(__dirname, 'src', 'main.js')),
-  output: {
-    path: path.resolve(path.join(__dirname, 'dist')),
-    filename: '[name].js'
-  },
-  resolve: {
-    extensions: [
-      '.js',
-      '.json',
-      '.css'
+    entry: path.resolve(path.join(__dirname, 'src', 'main.js')),
+    output: {
+        path: path.resolve(path.join(__dirname, 'dist')),
+        filename: '[name].js'
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.json',
+            '.css'
+        ]
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js[x]{0,1}$/,
+                exclude: /node_modules/,
+                use: [{loader: 'babel-loader'}]
+            }
+        ]
+    },
+    devtool: "source-map",
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: function (module) {
+                // this assumes your vendor imports exist in the node_modules directory
+                return module.context && module.context.indexOf('node_modules') !== -1;
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: isProd,
+            sourceMap: isDev
+        })
     ]
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js[x]{0,1}$/,
-        exclude: /node_modules/,
-        use: [{loader: 'babel-loader'}]
-      }
-    ]
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: function (module) {
-        // this assumes your vendor imports exist in the node_modules directory
-        return module.context && module.context.indexOf('node_modules') !== -1;
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: process.env.NODE_ENV === 'production'
-    })
-  ]
 };
