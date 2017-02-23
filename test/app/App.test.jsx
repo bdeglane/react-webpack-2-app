b9bae6144d0fb5083cbb1626efaa598044bd550b
Benoit Deglane <benoit.deglane@netapsys.fr>
2017-02-23 15:50:18 +0100

    setup mocha enzyme test

---
+++ test/app/App.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import App from '../../src/app/App.jsx';
+
+import {assert} from 'chai';
+import {shallow} from 'enzyme';
+
+describe('<App hello="world">', () => {
+    describe('valid rendered', () => {
+        const props = 'test';
+        const app = shallow(<App hello={props}/>);
+        describe('should render App react class', () => {
+            it('should have <h1/>', () => {
+                assert.equal(app.find('h1').length, 1);
+                const sentence = app.find('h1').children().nodes;
+                assert.equal(`${sentence.join('')}`, `Hello ${props}!`);
+            });
+            it('should have rendered props', () => {
+                assert.equal(app.contains(props), true);
+            });
+        });
+    });
+});
