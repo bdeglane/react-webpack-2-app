330f04a91e87b1d052c09dc6d77d777b440cc216
Benoit Deglane <benoit.deglane@netapsys.fr>
2017-02-24 17:24:23 +0100

    add component Header and style

--- src/app/component/header/Header.jsx
+++ src/app/component/header/Header.jsx
@@ -1,11 +1,13 @@
 import React, {Component, PropTypes} from 'react';
+import cx from'classnames';
 
 import grid from '../../style/component/grid.css';
+import styles from './Header.css';
 
 export default class Header extends Component {
     render() {
         return (
-            <header className={}>
+            <header className={cx(styles.header)}>
             </header>
         )
     }
--- src/app/component/layout/Layout.jsx
+++ src/app/component/layout/Layout.jsx
@@ -1,5 +1,6 @@
 import React, {Component, PropTypes} from 'react'
 import cx from 'classnames';
+import Header from '../header/Header.jsx';
 
 import grid from '../../style/component/grid.css';
 
@@ -9,6 +10,7 @@ export default class Layout extends Component {
             <div className={grid.frame}>
                 <div className={cx(grid.vertical, grid.block)}>
                     <div className={cx(grid.shrink, grid.block)}>
+                        <Header/>
                     </div>
                     <div className={grid.block}>
                         {this.props.children}
