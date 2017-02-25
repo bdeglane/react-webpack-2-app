af6349ebe91ee8539593dbc805682f27eeb2994f
Benoit Deglane <benoit.deglane@netapsys.fr>
2017-02-25 12:38:36 +0100

    add context provider with grid

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,6 @@
 import React, {Component, PropTypes} from 'react';
 import cx from 'classnames';
+import ContextProvider from './component/contextProvider/ContextProvider.jsx';
 import Layout from './component/layout/Layout.jsx';
 
 import styles from './style/main.css';
@@ -8,11 +9,13 @@ import grid from './style/component/grid.css';
 export default class App extends Component {
     render() {
         return (
-            <Layout>
-                <div className={cx(grid.block, styles.app)}>
-                    <h1 className={styles.title}>Hello {this.props.hello}!</h1>
-                </div>
-            </Layout>
+            <ContextProvider grid={grid}>
+                <Layout>
+                    <div className={cx(grid.block, styles.app)}>
+                        <h1 className={styles.title}>Hello {this.props.hello}!</h1>
+                    </div>
+                </Layout>
+            </ContextProvider>
         )
     }
 }
