f33dda82946a4b24f66ebc1668ec618b330d3588
Benoit Deglane <benoit.deglane@netapsys.fr>
2017-02-28 12:20:10 +0100

    use router to render app

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,7 @@
 import React, {Component, PropTypes} from 'react';
+import {browserHistory} from 'react-router'
 import cx from 'classnames';
+import Route from './route/Route.jsx';
 import ContextProvider from './component/contextProvider/ContextProvider.jsx';
 import Layout from './component/layout/Layout.jsx';
 
@@ -10,11 +12,13 @@ export default class App extends Component {
     render() {
         return (
             <ContextProvider grid={grid}>
-                <Layout>
-                    <div className={cx(grid.block, styles.app)}>
-                        <h1 className={styles.title}>Hello {this.props.hello}!</h1>
-                    </div>
-                </Layout>
+                <Route history={browserHistory}/>
+                {/*<Layout>
+                 <div className={cx(grid.block, styles.app)}>
+                 <h1 className={styles.title}>Hello {this.props.hello}!</h1>
+                 </div>
+                 </Layout>
+                 </Route>*/}
             </ContextProvider>
         )
     }
