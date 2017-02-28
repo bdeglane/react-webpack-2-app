dc09bbdd720ef529719940cd3ab0ec0398794816
Benoit Deglane <benoit.deglane@netapsys.fr>
2017-02-28 12:19:36 +0100

    prepare header add link

--- src/app/component/header/Header.jsx
+++ src/app/component/header/Header.jsx
@@ -1,15 +1,24 @@
 import React, {Component, PropTypes} from 'react';
+import {Link} from 'react-router';
 import cx from'classnames';
 
 import styles from './Header.css';
 
 export default class Header extends Component {
     render() {
+        const {grid} = this.context;
         return (
-            <header className={cx(styles.header)}>
+            <header className={cx(grid.block, styles.header)}>
+                <div className={cx(grid.block, grid.shrink)}></div>
+                <div className={cx(grid.block, styles.menu)}>
+                    <Link to="/">Home</Link>
+                </div>
             </header>
         )
     }
 }
 
 Header.propTypes = {};
+Header.contextTypes = {
+    grid: PropTypes.object
+};
