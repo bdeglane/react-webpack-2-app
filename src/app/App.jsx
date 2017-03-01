263bf43ded95863a95ed15ad831fbf7f0f49af19
Benoit Deglane <benoit.deglane@netapsys.fr>
2017-03-01 15:28:18 +0100

    clean class

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,7 @@
 import React, {Component, PropTypes} from 'react';
 import {browserHistory} from 'react-router'
-import cx from 'classnames';
 import Route from './route/Route.jsx';
 import ContextProvider from './component/contextProvider/ContextProvider.jsx';
-import Layout from './component/layout/Layout.jsx';
 
 import styles from './style/main.css';
 import grid from './style/component/grid.css';
@@ -13,17 +11,7 @@ export default class App extends Component {
         return (
             <ContextProvider grid={grid}>
                 <Route history={browserHistory}/>
-                {/*<Layout>
-                 <div className={cx(grid.block, styles.app)}>
-                 <h1 className={styles.title}>Hello {this.props.hello}!</h1>
-                 </div>
-                 </Layout>
-                 </Route>*/}
             </ContextProvider>
         )
     }
 }
-
-App.propTypes = {
-    hello: PropTypes.string.isRequired
-};
